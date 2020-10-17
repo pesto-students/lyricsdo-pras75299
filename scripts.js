6a98ffea09073cf241bede999dbbf4d9c45e8f04
pras75299 <singh.prashantking@gmail.com>
2020-10-17 16:25:13 +0530

    add validation to input

--- scripts.js
+++ scripts.js
@@ -1,8 +1,37 @@
 const searchLyrics = document.getElementById("searchlyrics");
+const form = document.getElementById('lyricsDo');
 const results = document.getElementById("lyricsResult");
 const apiUrl = "https://api.lyrics.ovh";
 const lyricsDiv = document.getElementById("lyrics");
 let timeoutSuggest;
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+  checkInputs();
+  onChange();
+});
+
+function checkInputs() {
+  const searchValue = searchLyrics.value.trim();
+  if(searchValue === '') {
+		setErrorFor(searchLyrics, 'Search Lyrics cannot be blank');
+	} else {
+		setSuccessFor(searchLyrics);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formGroup = input.parentElement;
+	const small = formGroup.querySelector('small');
+	formGroup.className = 'form-group error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formGroup = input.parentElement;
+	formGroup.className = 'form-group success';
+}
 
 function removeResults() {
    // results.remove(".lyricsResult");
@@ -18,8 +47,7 @@ function loadJSON(path, success, error) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
+                success(JSON.parse(xhr.responseText));
             } else {
                 if (error)
                     error(xhr);
@@ -29,7 +57,13 @@ function loadJSON(path, success, error) {
     xhr.open("GET", path, true);
     xhr.send();
 }
-
+function getLyrics(artist, title) {  
+  const lyricsUrl = apiUrl + '/' + artist + '/' + title;
+  loadJSON(lyricsUrl, function(data) {
+    console.log('get lyrics')
+  console.log(data);
+})
+}
 
 function suggestions() {
     let inputValue = searchLyrics.value;
@@ -37,7 +71,6 @@ function suggestions() {
       removeResults();
       return;
     }
-    console.log("Search suggestions for", inputValue);
     loadJSON(apiUrl + "/suggest/" + inputValue, function (data) {
       removeResults();
       let html;
@@ -47,7 +80,7 @@ function suggestions() {
                     <div class="album-image">
                       <img src="${item.album.cover_medium !== null ? item.album.cover_medium : './assets/default-placeholder-image.png'}"/>
                     </div>
-                    <a href="#" onclick="getLyrics(artist, title)">
+                    <a href="#" onclick="${getLyrics(item.artist.name, item.artist.title)}">
                       <p><span>Artist Name:</span> ${item.artist.name}</p>
                       <p><span>Album Name:</span> ${item.album.title}</p>
                       <p><span>Title:</span> ${item.title}</p>
@@ -56,14 +89,11 @@ function suggestions() {
             }
         });
         results.innerHTML = html;
-      console.log(data.data);      
+      //console.log(data.data);      
     });
 }
 
 
-function getLyrics(artist, title) {
-  loadJSON(apiUrl + '/' + artist + '/' + title, function(data1){
-    console.log(data1.data);
-  })
-}
+
+
 
